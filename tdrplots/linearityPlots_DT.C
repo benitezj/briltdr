8dc16d2dc0dfec0a0ea951fb62e378e904fe8609
Jose <jose.benitez@cern.ch>
2021-03-25 07:45:12 -0700

    DT linearity

---
+++ tdrplots/linearityPlots_DT.C
@@ -0,0 +1,20 @@
+#include "linearityPlots.C"
+
+void linearityPlots_DT()
+{
+  setTDRStyle();
+
+//// the following is already default, but can be modified here
+  lumi_sqrtS = "Run 324980 (2018)";
+  extraText  = "Preliminary";
+
+
+  plotLuminometer("input.root", "graph",  // input file , TGraphErrors name
+		  "DT Trigger Primitives (MB1-MB3)",                       //title
+		  0, 20000, "Inst. Luminosity (x10^{30} cm^{-2}s^{-1})",   // x-axis
+		  0, 4000000, "total trigger primitive rate (Hz)",           // y-axis
+		  5000,19000);                                         //fit range
+                                                                          
+
+  
+}
